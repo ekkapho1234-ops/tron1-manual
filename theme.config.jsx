e9ec9405fd0c13380120b5bbd556846d1e8378af
ekkaphon <ekkapho1289@gmail.com>
2026-01-10 11:03:45 +0700

    Upload TRON 1 Manual

---
+++ theme.config.jsx
@@ -0,0 +1,35 @@
+export default {
+  logo: (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+      <img src="/gic.png" alt="TRON 1 Logo" style={{ width: '50px', height: '30px' }} />
+      <span style={{ fontWeight: 470 }}>TRON1 Manual</span>
+    </div>
+  ),
+  project: {
+    link: 'https://github.com/limxdynamics',
+  },
+   navbar: {
+    extraContent: (
+      <a 
+        href="https://www.limxdynamics.com/en/tron1" 
+        target="_blank" 
+        rel="noreferrer"
+        style={{ 
+          display: 'flex', 
+          alignItems: 'center', 
+          padding: '0 5px' 
+        }}
+      >
+        <img 
+          src="/logo.png" 
+          alt="LimX" 
+          style={{ width: '70px', height: '25px', borderRadius: '4px' }} 
+        />
+      </a>
+    )
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
+  footer: {
+    text: 'TRON 1 Documentation ',
+  },
+}
